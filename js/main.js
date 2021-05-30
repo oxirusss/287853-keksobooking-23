7056f68b913d70057eea48d4c991d5fbb3fce012
oxirusss <parakhina.ksenia@protonmail.com>
2021-05-29 21:44:39 -0500

    Внесены правки в задание

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
-function randomNumber(min, max) {
-  if(max  <= min) {
-    throw Error('Число верхней границы меньше чем нижней');
-  }
-  return Math.floor(min + Math.random() * (max  - min));
-}
+const getRandomNumber = (min = 0, max = 0) => Math.floor(min + Math.random() * (max  - min));
+const getRandomCoordinates = (min = 0, max = 0, floats = 1) => (min + Math.random() * (max  - min)).toFixed(floats);
 
-function randomCoordinates(min, max, floats) {
-  if(max  <= min) {
-    throw Error('Число верхней границы меньше чем нижней');
-  }
-  const floatNumber = min + Math.random() * (max  - min);
-  return floatNumber.toFixed(floats);
-}
-
-randomNumber(10, 100);
-randomCoordinates(10, 100, 2);
+getRandomNumber(10, 100);
+getRandomCoordinates(10, 100, 2);
