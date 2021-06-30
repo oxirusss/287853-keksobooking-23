646a3a66e940cf84c1dcc72f43770cab4699253e
Ksenia Parakhina <oxirusss@gmail.com>
2021-06-30 03:05:56 -0500

    Внесены правки в задание Отрисуй

--- js/card.js
+++ js/card.js
@@ -1,51 +1,92 @@
-const cardTemplate = document.querySelector('#card').content;
-const newCardTemplate = cardTemplate.querySelector('.popup');
+const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const canvasMap = document.querySelector('#map-canvas');
-const housingType = {'palace': 'Дворец', 'flat': 'Квартира', 'bungalow': 'Бунгало', 'house': 'Дом', 'hotel': 'Отель'};
+const HousingType = {
+  PALACE: 'Дворец',
+  FLAT: 'Квартира',
+  BUNGALOW: 'Бунгало',
+  HOUSE: 'Дом',
+  HOTEL: 'Отель',
+};
+
+const createFeaturesElements = (data) => {
+  const featuresList = document.createDocumentFragment();
+  for (const item of data) {
+    const element = document.createElement('li');
+    element.classList.add('popup__feature', `popup__feature--${item}`);
+    featuresList.appendChild(element);
+  }
+  return featuresList;
+};
+
+const createPhotoElements = (data) => {
+  const photoList = document.createDocumentFragment();
+  for (const item of data) {
+    const photoElement = document.createElement('img');
+    photoElement.classList.add('popup__photo');
+    photoElement.setAttribute('src', item);
+    photoElement.setAttribute('alt', data.offer.type);
+    photoList.appendChild(photoElement);
+  }
+  return photoList;
+};
 
 const createCard = (data) => {
-  const card = newCardTemplate.cloneNode(true);
+  const card = cardTemplate.cloneNode(true);
 
-  const title = card.querySelector('.popup__title');
-  title.textContent = data.offer.title;
-
-  const address = card.querySelector('.popup__text--address');
-  address.textContent = data.offer.address;
-
-  const price = card.querySelector('.popup__text--price');
-  price.textContent = `${data.offer.price} ₽/ночь`;
-
-  const houseType = card.querySelector('.popup__type');
-  houseType.textContent = housingType[data.offer.type];
-
-  const capacity = card.querySelector('.popup__text--capacity');
-  capacity.textContent = `${data.offer.rooms} комнаты для ${data.offer.guests} гостей`;
-
-  const time = card.querySelector('.popup__text--time');
-  time.textContent = `Заезд после ${data.offer.checkin}, выезд до ${data.offer.checkout}`;
-
+  //Title check
+  if (data.offer.title) {
+    const title = card.querySelector('.popup__title');
+    title.textContent = data.offer.title;
+  }
+  //Address check
+  if (data.offer.address) {
+    const address = card.querySelector('.popup__text--address');
+    address.textContent = data.offer.address;
+  }
+  //Price check
+  if (data.offer.price) {
+    const price = card.querySelector('.popup__text--price');
+    price.textContent = `${data.offer.price} \u20bd/ночь`;
+  }
+  //Type check
+  if (data.offer.type) {
+    const houseType = card.querySelector('.popup__type');
+    houseType.textContent = HousingType[data.offer.type];
+  }
+  //Rooms check
+  if (data.offer.rooms && data.offer.guests) {
+    const capacity = card.querySelector('.popup__text--capacity');
+    capacity.textContent = `${data.offer.rooms} комнаты для ${data.offer.guests} гостей`;
+  }
+  if (data.offer.rooms) {
+    const capacityRooms = card.querySelector('.popup__text--capacity');
+    capacityRooms.textContent = `${data.offer.rooms} комнат`;
+  }
+  if (data.offer.guests) {
+    const capacityGuests = card.querySelector('.popup__text--capacity');
+    capacityGuests.textContent = `${data.offer.rooms} гостей`;
+  }
+  //Time check
+  if (data.offer.checkin && data.offer.checkout) {
+    const time = card.querySelector('.popup__text--time');
+    time.textContent = `Заезд после ${data.offer.checkin}, выезд до ${data.offer.checkout}`;
+  }
   // Features check
-  //Находим элемент
-  const features = card.querySelector('.popup__features');
-  //Проходим по массиву features, получаем массив уже с модификатором
-  const modifiers = data.offer.features.map((feature) => `popup__feature--${feature}`);
-  //Проверяем весь список, на наличие соответствующих элементов,
-  // если да, оставляем его, иначе удаляем. forEach - вызвана для каждого элемента, через нее выполним проверку соответствующего класса
-  features.querySelectorAll('.popup__feature').forEach((item) => {
-    //  Извлекаем из элемента второй класс
-    const modifier = item.classList[1];
-    // Проверка на соответствие
-    if (!modifiers.includes(modifier)) {
-      item.remove();
-    }
-  });
-
+  if (data.offer.features.length > 0) {
+    const featuresList = document.querySelector('.popup__features');
+    const featuresItems = createFeaturesElements(data.offer.features);
+    featuresList.appendChild(featuresItems);
+  }
+  // Description check
   const description = card.querySelector('.popup__description');
   description.textContent = data.offer.description;
-
-  const photos = card.querySelector('.popup__photos');
-  photos.src = data.offer.photos;
-
+  //Photos check
+  if (data.offer.photos.length > 0) {
+    const photoList = document.querySelector('.popup__photos');
+    const photoElement = createPhotoElements(data.offer.photos);
+    photoList.appendChild(photoElement);
+  }
+  //Avatar check
   const avatar = card.querySelector('img');
   avatar.src = data.author.avatar;
 };
