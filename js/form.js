b1014c49819e15f60c819170379ecd6a994da258
oxirusss <parakhina.ksenia@protonmail.com>
2021-07-07 02:00:51 -0500

    Отрисуй меня полностью (часть 2)

---
+++ js/form.js
@@ -0,0 +1,10 @@
+const  formAdState = () => {
+  const formAd = document.querySelector('.ad-form');
+  formAd.classList.toggle('ad-form--disabled');
+  formAd.toggleAttribute('disabled');
+  const mapFilters = document.querySelector('.map__filters');
+  mapFilters.classList.toggle('map__filters--disabled');
+  mapFilters.toggleAttribute('disabled');
+};
+
+export {formAdState};
--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import {generateRentList} from './data.js';
 import {renderCard} from './card.js';
+import {formAdState} from '../form';
 
 const data = generateRentList();
 renderCard(data[1]);
 data.length;
 
+const form = formAdState();
+form.length;
