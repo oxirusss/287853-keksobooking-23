7780e6bc09b513ff31217d6af6af9086a7763d70
oxirusss <parakhina.ksenia@protonmail.com>
2021-07-19 00:20:42 -0500

    Помощь друга Доработки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {generateRentList} from './data.js';
 import {createCard} from './card.js';
 import {initFormStartState} from '../form';
+import {renderingMap} from './map';
 
 const data = generateRentList();
 createCard(data[1]);
@@ -8,3 +9,5 @@ data.length;
 
 const form = initFormStartState();
 form.length;
+
+renderingMap();
